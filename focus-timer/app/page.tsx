6320cf9c1277fa6f4867e048b73a6eb534305e6e
qqaazz0222 <qqaazz0222@naver.com>
2024-03-01 20:00:36 +0900

    prototype ver.1.0

---
+++ focus-timer/app/page.tsx
@@ -0,0 +1,128 @@
+"use client";
+// 라이브러리
+import React, { useEffect, useRef, useState } from "react";
+import dynamic from "next/dynamic";
+const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
+// 컴포넌트
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+} from "@/components/ui/card";
+import { Slider } from "@/components/ui/slider";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+import Timer from "@/components/timer";
+// 아이콘
+import { VolumeX, Volume2, Play, Pause } from "lucide-react";
+
+export default function Home() {
+    const videoRef = useRef();
+    const [video, setVideo] = useState(
+        "https://www.youtube.com/embed/MZgO_k2VjMg"
+    );
+    const [videoState, setVideoState] = useState({
+        playing: false, // 재생중인지
+        muted: false, // 음소거인지
+        controls: false, // 기본으로 제공되는 컨트롤러 사용할건지
+        volume: 0.5, // 볼륨크기
+        playbackRate: 1.0, // 배속
+        played: 0, // 재생의 정도 (value)
+        seeking: false, // 재생바를 움직이고 있는지
+        duration: 0, // 전체 시간
+    });
+    const [timerState, setTimerState] = useState({
+        hour: 0,
+        minute: 30,
+        second: 0,
+    });
+    const genreList = ["발라드", "댄스", "어쿠스틱", "힙합", "팝", "Lo-Fi"];
+    const [genre, setGenre] = useState(genreList[0]);
+    const playPauseHandler = () => {
+        setVideoState({ ...videoState, playing: !videoState.playing });
+    };
+    const volumeChangeHandler = (e: any) => {
+        setVideoState({
+            ...videoState,
+            volume: e[0] / 100,
+            muted: e[0] === 0 ? true : false,
+        });
+    };
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center p-2">
+            <Card className="w-full max-w-[480px]">
+                <CardHeader>
+                    <div className="flex flex-row items-center gap-4">
+                        {videoState.volume === 0 ? (
+                            <VolumeX className="w-6 h-6" />
+                        ) : (
+                            <Volume2 className="w-6 h-6" />
+                        )}
+
+                        <Slider
+                            defaultValue={[50]}
+                            max={100}
+                            step={1}
+                            onValueChange={volumeChangeHandler}
+                        />
+                    </div>
+                </CardHeader>
+                <CardContent className="flex flex-col gap-4">
+                    <Timer state={timerState} setState={setTimerState} />
+                    <div className="flex flex-col gap-4 w-full">
+                        <div className="flex flex-row items-center gap-2">
+                            <Select>
+                                <SelectTrigger className="w-full">
+                                    <SelectValue placeholder="장르" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    {genreList.map((genre) => (
+                                        <SelectItem value={genre} key={genre}>
+                                            {genre}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
+
+                            {videoState.playing ? (
+                                <Button
+                                    variant="outline"
+                                    className="p-3"
+                                    onClick={playPauseHandler}
+                                >
+                                    <Pause className="w-4 h-4" />
+                                </Button>
+                            ) : (
+                                <Button
+                                    className="p-3"
+                                    onClick={playPauseHandler}
+                                >
+                                    <Play className="w-4 h-4" />
+                                </Button>
+                            )}
+                        </div>
+                    </div>
+                </CardContent>
+                <CardFooter>
+                    <ReactPlayer
+                        ref={videoRef}
+                        url={video} // 서버에서 받아온 video url
+                        playing={videoState.playing} // true = 재생중 / false = 멈춤
+                        controls={false} // 기본 컨트롤러 사용 x
+                        muted={videoState.muted} // 음소거인지
+                        volume={videoState.volume} // 소리조절 기능
+                        width="0"
+                        height="0"
+                    />
+                </CardFooter>
+            </Card>
+        </main>
+    );
+}
---
+++ focus-timer/components/timer.tsx
@@ -0,0 +1,150 @@
+import React, { useState, useEffect } from "react";
+import useCountDown from "react-countdown-hook";
+import { Input } from "@/components/ui/input";
+import { Button } from "./ui/button";
+import { Pause, Play, Square, StopCircle } from "lucide-react";
+
+interface TimerState {
+    [key: string]: number;
+}
+interface TimerProps {
+    state: TimerState;
+    setState: any;
+}
+
+const Timer = ({ state, setState }: TimerProps) => {
+    const initialTime =
+        (state.hour * 3600 + state.minute * 60 + state.second) * 1000;
+    const [isStart, setIsStart] = useState("stop");
+    const [timeLeft, { start, pause, resume, reset }] = useCountDown(
+        initialTime,
+        1000
+    );
+    const restart = React.useCallback(() => {
+        start(initialTime);
+    }, []);
+    useEffect(() => {
+        if (timeLeft === 0) {
+            setIsStart("stop");
+        }
+    }, [timeLeft]);
+    return (
+        <div id="timer" className="flex flex-col gap-2">
+            <div id="display" className="flex flex-row gap-2 items-center">
+                <Input
+                    id="pixel"
+                    className="text-center text-6xl h-fit"
+                    type="text"
+                    value={
+                        isStart === "stop"
+                            ? state.hour.toString().padStart(2, "0")
+                            : Math.floor(timeLeft / 1000 / 3600)
+                                  .toString()
+                                  .padStart(2, "0")
+                    }
+                    onChange={(e) => {
+                        setIsStart("stop");
+                        setState({ ...state, hour: parseInt(e.target.value) });
+                        reset();
+                    }}
+                />
+                :
+                <Input
+                    id="pixel"
+                    className="text-center text-6xl h-fit"
+                    type="text"
+                    value={
+                        isStart === "stop"
+                            ? state.minute.toString().padStart(2, "0")
+                            : Math.floor((timeLeft / 1000 / 60) % 60)
+                                  .toString()
+                                  .padStart(2, "0")
+                    }
+                    onChange={(e) => {
+                        setIsStart("stop");
+                        setState({
+                            ...state,
+                            minute: parseInt(e.target.value),
+                        });
+                        reset();
+                    }}
+                />
+                :
+                <Input
+                    id="pixel"
+                    className="text-center text-6xl h-fit"
+                    type="text"
+                    value={
+                        isStart === "stop"
+                            ? state.second.toString().padStart(2, "0")
+                            : ((timeLeft / 1000) % 60)
+                                  .toString()
+                                  .padStart(2, "0")
+                    }
+                    onChange={(e) => {
+                        setIsStart("stop");
+                        setState({
+                            ...state,
+                            second: parseInt(e.target.value),
+                        });
+                        reset();
+                    }}
+                />
+            </div>
+            <div
+                id="control"
+                className="flex flex-row gap-2 items-center justify-end"
+            >
+                {isStart === "stop" ? (
+                    <Button
+                        className="p-3"
+                        onClick={() => {
+                            setIsStart("start");
+                            start(initialTime);
+                        }}
+                    >
+                        <Play className="w-4 h-4" />
+                    </Button>
+                ) : (
+                    <>
+                        {isStart === "pause" ? (
+                            <Button
+                                variant="outline"
+                                className="p-3"
+                                onClick={() => {
+                                    setIsStart("start");
+                                    resume();
+                                }}
+                            >
+                                <Play className="w-4 h-4" />
+                            </Button>
+                        ) : (
+                            <Button
+                                variant="outline"
+                                className="p-3"
+                                onClick={() => {
+                                    setIsStart("pause");
+                                    pause();
+                                }}
+                            >
+                                <Pause className="w-4 h-4" />
+                            </Button>
+                        )}
+                        <Button
+                            variant="outline"
+                            className="p-3"
+                            onClick={() => {
+                                setIsStart("stop");
+                                reset();
+                            }}
+                        >
+                            <Square className="w-4 h-4" />
+                        </Button>
+                    </>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Timer;

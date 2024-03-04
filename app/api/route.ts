c3e0cfd38ddb04424682d5421726c127fc6df653
qqaazz0222 <qqaazz0222@naver.com>
2024-03-04 17:59:14 +0900

    prototype ver.1.4.2

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,11 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import Axios from "axios";
 
+const API_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
+
 export async function GET(req: NextRequest, res: NextResponse) {
     const genre = req.nextUrl.searchParams.get("g");
     const key = encodeURI(`${genre} 플레이리스트`);
     const response = await Axios.get(
-        `https://www.googleapis.com/youtube/v3/search?key=AIzaSyBCV424E5phcYZNF8Pk18qXTigKe-2O4jE&part=id&maxResults=10&q=${key}`
+        `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&part=id&maxResults=10&q=${key}`
     );
     const result =
         response.data.items[Math.floor(Math.random() * 10)].id.videoId;
